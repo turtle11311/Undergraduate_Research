fe5219af7a5b4fe3d15a4ebc03a31481812f24f0
TurtleBee <turtle11311@gmail.com>
2016-11-12 18:23:31 +0800

    fix for some compiler

--- src/Gate.cpp
+++ src/Gate.cpp
@@ -6,6 +6,6 @@ Gate::Gate(const char* const name)
 
 void Gate::addInput(Gate *input, int thresholdVal)
 {
-    this->fan_in.push_back({input, thresholdVal, false});
+    this->fan_in.push_back(ThresholdInput{input, thresholdVal, false});
     input->fan_out.push_back(this);
 }
